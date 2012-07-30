d759540e50a6b46a92a3c74c16e8986a4c534f8c
xoomdev <xoomdevnet@gmail.com>
2012-07-29 22:58:51 -0400

    Add theming of lockscreen and fix reboot dialog not theming.
    Change-Id: Ie0aabd4d58b11eac02d0ea116a90d983316145b5

--- policy/src/com/android/internal/policy/impl/GlobalActions.java
+++ policy/src/com/android/internal/policy/impl/GlobalActions.java
@@ -1082,7 +1082,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
         final String[] rebootOptions = mContext.getResources().getStringArray(R.array.shutdown_reboot_options);
         final String[] rebootReasons = mContext.getResources().getStringArray(R.array.shutdown_reboot_actions);
 
-        AlertDialog d = new AlertDialog.Builder(mContext)
+        AlertDialog d = new AlertDialog.Builder(getUiContext())
                 .setSingleChoiceItems(rebootOptions, 0,
                         new DialogInterface.OnClickListener() {
                             @Override
